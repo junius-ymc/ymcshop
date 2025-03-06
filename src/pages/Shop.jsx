f347a471530bcce04df6920703210530b49b736f
unknown <juymc1974@gmail.com>
2025-03-06 23:19:42 +0700

    ymcshop

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,16 +3,21 @@ import useEcomStore from '../store/ecom-store';
 import ProductCard from '../components/card/ProductCard';
 import SearchCard from '../components/card/SearchCard';
 import { useTranslation } from "react-i18next"; // ✅ เพิ่มตัวช่วยแปลภาษา
+import { Loader } from 'lucide-react';
 
 const Shop = () => {
   const getProduct = useEcomStore((state) => state.getProduct);
   const products = useEcomStore((state) => state.products);
   const { t } = useTranslation(); // ✅ ใช้ตัวช่วยแปลภาษา
   // เรียงลำดับผลลัพท์จากใหม่ไปเก่า
-  // const sortedProducts = [...products].sort((a, b) => b.id - a.id);
+  const sortedProducts = [...products].sort((a, b) => b.id - a.id);
+
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
+    // setIsLoading(true);
     getProduct();
+    setIsLoading(false);
   }, []);
 
   return (
@@ -29,7 +34,10 @@ const Shop = () => {
 
             <div className="div-content shop-product-cart">
               {
-                products.map((item, index) =>
+                isLoading && <Loader className='w-16 h-16 animate-spin' />
+              }
+              {
+                sortedProducts.map((item, index) =>
                   <ProductCard key={index} item={item} />
                 )
               }
