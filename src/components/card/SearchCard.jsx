5fea904a151957b52fc53990cf5aa3a8d1dc6dd5
unknown <juymc1974@gmail.com>
2025-03-16 22:02:04 +0700

    ymcshop-edit-css

--- src/components/card/SearchCard.jsx
+++ src/components/card/SearchCard.jsx
@@ -28,9 +28,9 @@ const SearchCard = () => {
   const [ok, setOk] = useState(false);
 
   // console.log(categories)
-  // useEffect(() => {
-  //   getCategory();
-  // }, []);
+  useEffect(() => {
+    getCategory();
+  }, []);
 
   // Step 1 Search Text
   // console.log(text)
