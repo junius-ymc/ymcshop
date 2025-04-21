eefcf3283a76f871444aa35f479a682dfa2f14ed
unknown <juymc1974@gmail.com>
2025-04-22 04:14:47 +0700

    ymcshop-Add-ReviewAndDashboard

---
+++ src/pages/admin/AllReviews.jsx
@@ -0,0 +1,13 @@
+// rafce
+import React from 'react'
+import TableReviews from '../../components/admin/TableReviews'
+
+const AllReviews = () => {
+  return (
+    <div>
+        <TableReviews />
+    </div>
+  )
+}
+
+export default AllReviews
