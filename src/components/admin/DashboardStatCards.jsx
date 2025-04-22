caad0fff919ac42e2d151533c0a5dc44bffc5b90
unknown <juymc1974@gmail.com>
2025-04-22 22:33:15 +0700

    ymcshop-Edit-DashboardStatCards

--- src/components/admin/DashboardStatCards.jsx
+++ src/components/admin/DashboardStatCards.jsx
@@ -7,11 +7,14 @@ const DashboardStatCards = ({ stats }) => {
     <div className="admin-dashboard-stats-grid">
       <div className="admin-dashboard-stat-card">
         <div className="stat-icon bg-blue-500">
-        {stats.ordersToday > 0 && (
-          <div className="relative">
-            <div className="admin-notifycart-1">{stats.ordersToday}</div>
-          </div>
-        )}
+          {stats.ordersToday > 0 && (
+            <div className="tooltip-wrapper">
+              <div className="relative">
+                <div className="admin-notifycart-1">{stats.ordersToday}</div>
+              </div>
+              <div className="tooltip-1">ออเดอร์ใหม่วันนี้</div>
+            </div>
+          )}
           <ShoppingCart />
         </div>
         <div className="stat-text">
@@ -21,11 +24,14 @@ const DashboardStatCards = ({ stats }) => {
       </div>
       <div className="admin-dashboard-stat-card">
         <div className="stat-icon bg-green-500">
-        {stats.usersToday > 0 && (
-          <div className="relative">
-            <div className="admin-notifycart-1">{stats.usersToday}</div>
-          </div>
-        )}
+          {stats.usersToday > 0 && (
+            <div className="tooltip-wrapper">
+              <div className="relative">
+                <div className="admin-notifycart-1">{stats.usersToday}</div>
+              </div>
+              <div className="tooltip-1">ยุสเซอร์ใหม่วันนี้</div>
+            </div>
+          )}
           <Users />
         </div>
         <div className="stat-text">
@@ -44,11 +50,14 @@ const DashboardStatCards = ({ stats }) => {
       </div>
       <div className="admin-dashboard-stat-card">
         <div className="stat-icon bg-red-500">
-        {stats.contactsToday > 0 && (
-          <div className="relative">
-            <div className="admin-notifycart-1">{stats.contactsToday}</div>
-          </div>
-        )}
+          {stats.contactsToday > 0 && (
+            <div className="tooltip-wrapper">
+              <div className="relative">
+                <div className="admin-notifycart-1">{stats.contactsToday}</div>
+              </div>
+              <div className="tooltip-1">ติดต่อเข้ามาใหม่วันนี้กดเกดเกดเกด</div>
+            </div>
+          )}
           <Mail />
         </div>
         <div className="stat-text">
@@ -58,11 +67,14 @@ const DashboardStatCards = ({ stats }) => {
       </div>
       <div className="admin-dashboard-stat-card">
         <div className="stat-icon bg-sky-500">
-        {stats.reviewsToday > 0 && (
-          <div className="relative">
-            <div className="admin-notifycart-1">{stats.reviewsToday}</div>
-          </div>
-        )}
+          {stats.reviewsToday > 0 && (
+            <div className="tooltip-wrapper">
+              <div className="relative">
+                <div className="admin-notifycart-1">{stats.reviewsToday}</div>
+              </div>
+              <div className="tooltip-1">รีวิวสินค้าใหม่วันนี้</div>
+            </div>
+          )}
           <MessageSquareDiff />
         </div>
         <div className="stat-text">
