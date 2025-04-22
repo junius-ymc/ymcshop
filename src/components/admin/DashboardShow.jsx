badf79bf4b7ee76a89d587af910e127824e0f7c3
unknown <juymc1974@gmail.com>
2025-04-22 20:11:16 +0700

    ymcshop-Edit-DashboardShow

--- src/components/admin/DashboardShow.jsx
+++ src/components/admin/DashboardShow.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import DashboardStatCards from "./DashboardStatCards";
 import { getDashboardStats } from "../../api/admin"; // API ดึงสถิติหลังบ้าน
-import useEcomStore from '../../store/ecom-store'
+import useEcomStore from '../../store/ecom-store';
+import LoaderDiv from "../LoaderDiv";
 
 const DashboardShow = () => {
   const token = useEcomStore((state) => state.token);
+  const [loading, setLoading] = useState(false);
   const [stats, setStats] = useState({
     orders: 0,
     users: 0,
@@ -14,18 +16,23 @@ const DashboardShow = () => {
   });
 
   useEffect(() => {
+    setLoading(true);
     getDashboardStats(token)
       .then((res) => {
         setStats(res.data);
       })
       .catch((err) => {
         console.error("❌ Error loading stats:", err);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
 
   return (
     <div className="div-main-admin-content">
       <h2 className="admin-title">📊 Dashboard</h2>
+      {loading && (<div><LoaderDiv /></div>)}
       <DashboardStatCards stats={stats} />
       {/* เพิ่มอย่างอื่นต่อได้ เช่น Chart, Table, Notification */}
     </div>
