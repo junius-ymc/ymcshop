2012397edf665c04c315fa158da47c0f87b7ae70
unknown <juymc1974@gmail.com>
2025-04-30 03:43:09 +0700

    ymcshop-Edit-dateFormat

--- src/utils/dateformat.jsx
+++ src/utils/dateformat.jsx
@@ -1,7 +1,12 @@
 import moment from "moment/min/moment-with-locales";
 
+export const dateFormat = (date) => {
+    const mDate = moment(date); // เรียก moment() เพียงครั้งเดียว
+    if (!mDate.isValid()) return "Invalid date";
 
-export const dateFormat = (date) =>{
-    // return moment(date).locale('en').format('LL')
-    return moment(date).locale(localStorage?.language || 'en').format('ll')
+    const years = mDate.format('YYYY');
+    const lang = localStorage?.language || 'en';
+    const thYear = lang === "th" ? Number(years) + 543 : years;
+
+    return mDate.locale(lang).format(`D MMM ${thYear}`);
 }
