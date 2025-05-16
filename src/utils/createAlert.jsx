9acab5e2e9864ed711cd722d2e2c52fd66a25b16
unknown <juymc1974@gmail.com>
2025-05-16 19:39:13 +0700

    ymcshop-Add-sweetalert2

---
+++ src/utils/createAlert.jsx
@@ -0,0 +1,22 @@
+import Swal from "sweetalert2";
+
+export const createAlert = (icon, text, confirmButtonText, timer) => {
+  return Swal.fire({
+    icon: icon || "info",
+    text: text || "Something Wrong!!",
+    confirmButtonText: confirmButtonText || "OK",
+    timer: timer || 3000,
+  });
+};
+
+export const createNofity = (icon, title, text, confirmButtonText, timer) => {
+  return Swal.fire({
+    position: "top",
+    icon: icon,
+    title: title,
+    text: text,
+    confirmButtonText: confirmButtonText || "OK",
+    // showConfirmButton: false,
+    timer: timer || 3000,
+  });
+};
